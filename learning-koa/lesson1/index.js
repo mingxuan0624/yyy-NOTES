82bf0d9d80ea81603bdbdc3f0fdaf7489f1d39ae
mingxuan0624 <13166372117@163.com>
2019-01-16 15:46:42 +0800

    'koa环境搭建'

---
+++ learning-koa/lesson1/index.js
@@ -0,0 +1,14 @@
+//用require引入koa
+const Koa = require('koa');
+
+//创建一个Koa对象来表示web app本身
+const app = new Koa();
+
+//对于任何请求,app将调用async异步函数来处理请求.
+app.use( async(ctx) => {
+    ctx.body = 'It is my first koa page!'
+})
+
+//进行监听
+app.listen(3000);
+console.log('app is strating at port 3000')
